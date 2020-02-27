b62f1556eb63bd61a5e28da37e8122927a18b0ad
momonnga25252 <momonnga25252@gmail.com>
2020-02-27 22:37:13 +0900

    Decoration変更

--- src/main/java/jp/momonnga/spigot/module/inventory/InventoryDecoration.java
+++ src/main/java/jp/momonnga/spigot/module/inventory/InventoryDecoration.java
@@ -2,22 +2,30 @@
 
 import org.bukkit.inventory.ItemStack;
 
-public class InventoryDecoration extends CustomInventory {
+public class InventoryDecoration{
+
+    private CustomInventory inventory;
+
+    public InventoryDecoration(){}
 
     public InventoryDecoration (CustomInventory editor) {
-        super(editor);
+        inventory = editor;
     }
 
-    public void drawVerticalLine(int x ,ItemStack itemStack){
+    public CustomInventory getInventory() {
+        return inventory;
+    }
+
+    public void drawVerticalLine(int x , ItemStack itemStack){
         for(int y = 0;;y++) {
-            if(x + (9 * y) > super.getSize()) break;
-            super.setItem(x,y,itemStack.clone());
+            if(x + (9 * y) > inventory.getSize()) break;
+            inventory.setItem(x,y,itemStack.clone());
         }
     }
 
     public void drawHorizontalLine(int y,ItemStack itemStack){
         for(int i = 0;i < 9;i++){
-            super.setItem(i,y,itemStack.clone());
+            inventory.setItem(i,y,itemStack.clone());
         }
     }
 
@@ -25,7 +33,7 @@ public class InventoryDecoration extends CustomInventory {
         drawHorizontalLine(0,itemStack);
         drawHorizontalLine(8,itemStack);
         drawVerticalLine(0,itemStack);
-        drawVerticalLine( (super.getSize()+1)/ 9,itemStack);
+        drawVerticalLine( (inventory.getSize()+1)/ 9,itemStack);
     }
 
 }
